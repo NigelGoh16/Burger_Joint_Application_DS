8419da4b6418761d6d1222685872cdf3ec8ea976
xnigel <nigell.goh@gmail.com>
2022-06-06 23:02:00 +0800

    Progress 1

--- Burger joint.cc
+++ Burger joint.cc
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <stdlib.h>
+#include <iterator>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
+
+vector<string> v;
+time_t now = time(0);
+tm *ltm = localtime(&now);
+
+void ordering(int ord, int qty){
+    if (ord == 0 || qty == 0){
+        return;
+    }
+    else if (ord == 1){
+        vector<string>::iterator itr;
+        for(itr=v.begin();itr!=v.end();itr++)
+        {
+            if (*itr == "Chicken burger"){
+                
+            } 
+        }
+
+        v.push_back("Chicken burger");
+        v.push_back(to_string(qty));
+    }
+    else if (ord == 2)
+    {
+        v.push_back("Beef burger");
+        v.push_back(to_string(qty));
+    }
+    else if (ord == 3)
+    {
+        v.push_back("Coca cola");
+        v.push_back(to_string(qty));
+    }
+    else if (ord == 4)
+    {
+        v.push_back("Pepsi");
+        v.push_back(to_string(qty));
+    }
+    else
+    {
+        cout <<"You have entered an invalid number please try again";
+        cout << "\nFood/Drinks: ";
+        cin >> ord;
+        cout << "\nQuantity: ";
+        cin >> qty;
+        ordering(ord, qty);
+    }
+}
 
 int main() {
     int choice, order, qty, function, payment;
@@ -21,24 +74,48 @@ int main() {
             if (choice == 1){
                 cout << "Enter the order details (Chicken burger id = 1, Beef burger id = 2, Coca cola id = 3, Pepsi id = 4):\n";
                 
-                //In the while function need to include a function to store the data
-                
                 while (order != 0){
                     cout << "Food/Drinks: ";
                     cin >> order;
                     cout << "\nQuantity: ";
                     cin >> qty;
                     cout << "Enter 0 to complete your order";
+                    
+                    ordering(order, qty);
                 }
+
+                fstream file;
+                file.open("Order.txt",ios_base::out);
+                
+                vector<string>::iterator itr;
+                    
+                file << 1900 + ltm->tm_year << '/' << 1 + ltm->tm_mon << '/' << ltm->tm_mday << ", ";
+                file << ltm->tm_hour << ":" << setfill('0') << setw(2) <<ltm->tm_min << ", ";
+
+                for(itr=v.begin();itr!=v.end();itr++)
+                {
+                    file << *itr << ", ";
+                }
+            
+                file.close();
                 cout << "You have successfully completed your order.\n";
             }
             else if (choice == 2)
             {
-                cout << "Order ID, Time, Date, Amount";
+                cout << "\n\nOrder ID,\tDate,\tTime,\tAmount\n";
                 
-                //Need to include a function to call the stored data
+                ifstream file("Order.txt");
+                string data = "";
+                int id = 1;
+                while (getline(file, data))
+                {
+                    cout << setfill('0') << setw(4) << id << "\t";
+                    cout << data;
+                    id++;
+                }
+                file.close();
 
-                cout << "Functions:\n" << "1. Search Order\n" << "2. Sort Orders\n" << "3. Cancel Order\n";
+                cout << "\nFunctions:\n" << "1. Search Order\n" << "2. Sort Orders\n" << "3. Cancel Order\n";
                 cout << "Enter the function in numbers: ";
                 cin >> function;
 
